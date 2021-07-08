12db3151e9765a45dfd6449b0454651a574ac012
LordAvorio <gadingramjets@gmail.com>
2021-07-08 20:50:27 +0700

    Tipe Data Updated

---
+++ src/Array.java
@@ -0,0 +1,29 @@
+public class Array {
+    public static void main(String[] args) {
+        String[] stringArray;
+        stringArray = new String[3];
+
+        String[] stringArray2 = new String[3];
+
+        stringArray[0] = "Eko";
+        stringArray[1] = "Sulaiman";
+        stringArray[2] = "Robust";
+
+        // System.out.println(stringArray[0]);
+
+        String[] namaNama = {
+            "Gading", "Condro", "Prakoso"
+        };
+
+        System.out.println(namaNama.length);
+
+        //Array 2 dimensi
+        String[][] members = {
+            {"Susan","Hayek"},
+            {"Marco","Tropoja"},
+            {"Louvre"}
+        };
+
+
+    }   
+}
---
+++ src/Variable.java
@@ -0,0 +1,20 @@
+public class Variable {
+    public static void main(String[] args) {
+        String name;
+        name = "Gading Condro Prakoso";
+
+        int age = 30;
+        String address = "Indonesia";
+
+        var company = "IndoGraha";
+
+        final double phi = 3.14;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(address);
+        System.out.println(phi);
+
+
+    }
+}
